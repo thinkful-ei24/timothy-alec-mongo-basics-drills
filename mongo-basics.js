948ff63fde088b58ba6cae34ac4c8dc198aa5956
Timothy Chang <superfluous11@gmail.com>
2018-09-17 08:36:12 -0700

    Completed get and limit & sort exercises

---
+++ mongo-basics.js
@@ -0,0 +1,6 @@
+// get all restaurants
+// db.restaurants.find();
+
+//limit and sort
+//display the first 10 results when restaurants is sorted by name
+//db.restaurants.find().sort({name: 1}).limit(10).pretty(); 
